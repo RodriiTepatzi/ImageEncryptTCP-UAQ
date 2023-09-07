31cdbd11d912bb34793dd7d8ffdf03d1b0c763c0
RodriiTepatzi <rodrif19@gmail.com>
2023-09-07 13:58:55 -0600

    adding the client side

---
+++ EncryptionManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEncryptTCP
+{
+	public class EncryptionManager
+	{
+		public static List<int> EncryptImage(string path)
+		{
+			byte[] key = Encoding.ASCII.GetBytes("2k");
+			byte[] image = File.ReadAllBytes(path);
+
+			List<int> EncrypMessage = RC4(image, key);
+
+			return EncrypMessage;
+
+			//File.WriteAllBytes(path, EncrypMessage.ConvertAll(b => (byte)b).ToArray());
+		}
+
+		static List<int> KSA(byte[] key)
+		{
+
+			//Llenamos K con la key
+			//Si no cabe la repetimos
+			List<int> K = new List<int>();
+
+			int KeyLength = key.Length;
+			int i = 0;
+			int j = 0;
+
+			while (i < 256)
+			{
+
+				if (j == KeyLength)
+				{
+					j = 0;
+				}
+
+				K.Add(key[j]);
+				i++;
+				j++;
+
+			}
+
+			List<int> S = new List<int>(256);
+			for (i = 0; i < 256; i++)
+			{
+
+				S.Add(i);
+
+			}
+
+			j = 0;
+			for (i = 0; i < 256; i++)
+			{
+
+				j = (j + S[i] + K[i]) % 256;
+
+				int aux = S[i];
+				S[i] = S[j];
+				S[j] = aux;
+
+			}
+
+			return S;
+
+		}
+
+		static List<int> KeyStream(List<int> S, int DataLength)
+		{
+
+			List<int> KeyStream = new List<int>();
+
+			int i = 0;
+			int j = 0;
+
+			for (int k = 0; k < DataLength; k++)
+			{
+
+				i = (i + 1) % 256;
+				j = (j + S[i]) % 256;
+
+				int aux = S[i];
+				S[i] = S[j];
+				S[j] = aux;
+
+				int t = (S[i] + S[j]) % 256;
+				KeyStream.Add(S[t]);
+
+			}
+
+			return KeyStream;
+		}
+
+		static List<int> RC4(byte[] data, byte[] key)
+		{
+			List<int> S = KSA(key);
+			List<int> K = KeyStream(S, data.Length);
+			List<int> EncrypMessage = new List<int>();
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				EncrypMessage.Add(data[i] ^ K[i]);
+			}
+
+			return EncrypMessage;
+		}
+	}
+}
---
+++ Manager/ConnectionManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ImageEncryptTCP.Manager
+{
+	public class ConnectionManager
+	{
+		private static ConnectionManager _instance = new ConnectionManager();
+		public static ConnectionManager Instance { get { return _instance; } }
+		public string? ToIPAddress { get; set; }
+		public string? Port { get; set; }
+		public string? ImagePath { get; set; }
+
+		private ConnectionManager() 
+		{
+
+		}
+
+
+		static async void StartClient()
+		{
+			try
+			{
+				//Creacion del socket
+				if(Instance.ToIPAddress == null)
+				{
+					throw new Exception();
+				}
+
+                if(Instance.Port == null)
+                {
+					throw new Exception();
+                }
+
+				if (Instance.ImagePath == null)
+				{
+					throw new Exception();
+				}
+
+				Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse(Instance.ToIPAddress), int.Parse(Instance.Port));
+
+				//Se conecta el cliente al socket
+				Console.WriteLine("Conectando al servidor...");
+				await client.ConnectAsync(remoteEP);
+				Console.WriteLine("Conectado al servidor {0}", remoteEP);
+
+				while (true)
+				{
+					//Se convierte la imagen en bytes
+					byte[] img = File.ReadAllBytes(Instance.ImagePath);
+					//Ingresa la llave
+					Console.WriteLine("Ingresa la llave: ");
+					String key = Console.ReadLine();
+					//Se convierte la llave en bytes
+					byte[] keyb = Encoding.ASCII.GetBytes(key);
+
+					var encryptedImg = EncryptionManager.EncryptImage(Instance.ImagePath);
+					List<char> imgb = encryptedImg.Select(i => (char)i).ToList();
+					byte[] imgEb = Encoding.ASCII.GetBytes(imgb.ToArray());
+
+					//Se convinan los bytes para poder mandarlos como uno solo
+					byte[] combinedData = new byte[sizeof(int) + imgEb.Length + keyb.Length];
+					BitConverter.GetBytes(imgEb.Length).CopyTo(combinedData, 0);
+					imgEb.CopyTo(combinedData, sizeof(int));
+					keyb.CopyTo(combinedData, sizeof(int) + imgEb.Length);
+
+					//Se envian los bytes combinados
+					client.Send(combinedData);
+
+					Console.WriteLine("Datos enviados");
+					break;
+				}
+
+				client.Shutdown(SocketShutdown.Both);
+				client.Close();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
+
+
+
+	}
+}
